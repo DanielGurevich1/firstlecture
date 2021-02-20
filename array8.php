31d97f5c1f48416f8ba07cf33d58c8a033e4fbe7
DanielGurevich1 <73877308+DanielGurevich1@users.noreply.github.com>
2021-02-20 14:47:20 +0200

    almost9

--- array8.php
+++ array8.php
@@ -8,14 +8,14 @@ echo '<br>'.'<h2>task 5.2.8</h2>';
 for ($i=0; $i < 3; $i++) { 
     $num = rand(0,5);
     echo $num.', ';
-    if ($num == 0) {
-        continue;
-        
-    }
     for ($j=0; $j < $num; $j++) { 
         $int = rand(0,10);
         $masyvas[$i][$j] = $int;
         
+    }
+    if ($num == 0) {
+        $masyvas[$i][$j] = rand(0, 10);
+
     }
 }
 echo '<pre>';
@@ -23,3 +23,29 @@ print_r($masyvas) ;
 
 echo '<br>'.'<h2>task 5.2.9</h2>';
 // 9.	Paskaičiuokite 8 uždavinio masyvo visų reikšmių sumą ir išrūšiuokite masyvą taip, kad pirmiausiai eitų mažiausios masyvo reikšmės arba jeigu reikšmė yra masyvas, to masyvo reikšmių sumos.
+$sumArray = [];
+foreach ($masyvas as $i=>$subArray) {
+  foreach ($subArray as $j=>$value) {
+    $sumArray[$i]+=$value;
+  }
+}
+
+print_r($sumArray);
+echo array_sum($sumArray).'<br><br><br>';
+//sort
+sort($sumArray);
+// usort($sumArray, function($a, $b){
+//     return $a[$i] <=> $b[$i];
+// });
+
+print_r($sumArray);
+
+$sortedArray = [];
+foreach ($masyvas as $i=>$subArray) {
+  foreach ($subArray as $j=>$value) {
+    $sumArray[$i]+=$value;
+  }
+}
+
+
+
--- penkta_astunta_uzd.php
+++ penkta_astunta_uzd.php
@@ -3,19 +3,39 @@ echo '<br>'.'<h2>task 5.2.5</h2>';
 // Sukurkite masyvą iš 30 elementų. Kiekvienas masyvo elementas yra masyvas [user_id => xxx, place_in_row => xxx] user_id atsitiktinis unikalus skaičius nuo 1 iki 1000000, place_in_row atsitiktinis skaičius nuo 0 iki 100. 
 $elements = 2;
 for ($i=0; $i < $elements; $i++) { 
-    
-    $rands = [];
-    $num = false;
-    while(!$num) {
-        $userIdMax = 1000000;
-        $userId = mt_rand(0,$userIdMax);
-        $num = !in_array($userId, $rands) ;
+
+    $uniq = [];
+    $uniq1 = [];
+    $userIdMax = 5; // max value
+        $userId = mt_rand(0, $userIdMax);
+    foreach($uniq as $userId){
+        if (!in_array($userId, $uniq)){
+            array_push($uniq1, $userId);
+        }
     }
+    // $rands = [];
+    // $num = false;
+    // while(!$num) {
+    //     $userIdMax = 5; // max value
+    //     $userId = mt_rand(0, $userIdMax); // sukurtas elementas
+    //     array_push($rands, $userId); // inserted into array rands
+    //     print_r($rands);
+    //     $num = !in_array($userId, $rands) ; // ar userID yra rands
+    // }
     $array55[] = ['user_id'=>$userId, 'place_in_row'=>rand(0,100)];
+   
+    if($array55['user_id'] == $array55['user_id'+1]){
+        $tempNum = $array55[$i];
+        $array55[$i] = $array55[$i+ 1];
+        $array55[$i+1] = $tempNum;
+    }
 
 }
 echo '<pre>';
 print_r($array55);
+
+echo '</pre>';
+
 
 echo '<br>'.'<h2>task 5.2.6</h2>';
 // 6.	Išrūšiuokite 5 uždavinio masyvą pagal user_id didėjančia tvarka. Ir paskui išrūšiuokite pagal place_in_row mažėjančia tvarka.
